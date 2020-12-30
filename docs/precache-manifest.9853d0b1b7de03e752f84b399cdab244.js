fd7496983f31b3337287e4dcfc8e1e9004210d03
Biljana <zabiljanupavlovic@gmail.com>
2020-12-30 16:41:32 +0100

    docs folder

---
+++ docs/precache-manifest.9853d0b1b7de03e752f84b399cdab244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7520e5825dd84697d43b1b549082a076",
+    "url": "/Quotes/index.html"
+  },
+  {
+    "revision": "dbe66d772501f117415b",
+    "url": "/Quotes/static/css/main.fd8274a0.chunk.css"
+  },
+  {
+    "revision": "f63f995f623a37c9f31e",
+    "url": "/Quotes/static/js/2.9b62f103.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Quotes/static/js/2.9b62f103.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbe66d772501f117415b",
+    "url": "/Quotes/static/js/main.983a0dce.chunk.js"
+  },
+  {
+    "revision": "3c4e5d8018a35d96c15d",
+    "url": "/Quotes/static/js/runtime-main.4784090c.js"
+  }
+]);
